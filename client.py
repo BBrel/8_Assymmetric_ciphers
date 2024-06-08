0f94015ed9371c7c7a3def7fd4dc508533a7ccb3
BBrel <vbrelgin@gmail.com>
2024-06-08 19:23:33 +0300

    my_version

--- client.py
+++ client.py
@@ -1,14 +1,40 @@
 import socket
-import pickle
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
-HOST = '127.0.0.1'
-PORT = 8080
 
-sock = socket.socket()
-sock.connect((HOST, PORT))
+class Client:
+    def __init__(self, host='localhost', port=8889):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+        self.keys = RSA.generate(2048)
+        self.cipher = PKCS1_OAEP.new(self.keys)  # класс для расшифровки в библиотеке Crypto
 
-p, g, a = 7, 5, 3
-A = g ** a % p
-sock.send(pickle.dumps((p, g, A)))
+    def send_message(self):
+        # Отправка открытого ключа серверу
+        self.client_socket.send(self.keys.publickey().export_key())
 
-sock.close()
+        # Получение открытого ключа сервера
+        server_pub_key_data = self.client_socket.recv(2048)
+        server_pub_key = RSA.import_key(server_pub_key_data)
+        server_cipher = PKCS1_OAEP.new(server_pub_key)
+
+        # Шифровка и отправка сообщения
+        encrypted_msg = server_cipher.encrypt(b"hello server!")
+        self.client_socket.send(encrypted_msg)
+
+        # Получение и расшифровка обратного сообщения от сервера
+        encrypted_reply = self.client_socket.recv(2048)
+        print(f'Зашифрованный ответ сервера: {encrypted_reply}')
+        reply = self.cipher.decrypt(encrypted_reply)
+        print("расшифрованный ответ от сервера:", reply.decode())
+
+    def client_start(self):
+        self.client_socket.connect((self.host, self.port))
+
+
+# Запуск клиента
+client = Client()
+client.client_start()
+client.send_message()
--- server.py
+++ server.py
@@ -1,15 +1,46 @@
 import socket
-import pickle
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP
 
-HOST = '127.0.0.1'
-PORT = 8080
 
-sock = socket.socket()
-sock.bind((HOST, PORT))
-sock.listen(1)
-conn, addr = sock.accept()
+class Server:
+    def __init__(self, host='localhost', port=8889):
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(1)
+        self.keys = RSA.generate(2048)
+        self.cipher = PKCS1_OAEP.new(self.keys)  # класс для расшифровки в библиотеке Crypto
 
-msg = conn.recv(1024)
-print(pickle.loads(msg))
+    def handle_client(self, client_socket):
+        # Получение открытого ключа клиента
+        client_pub_key_data = client_socket.recv(2048)
+        client_pub_key = RSA.import_key(client_pub_key_data)
+        client_cipher = PKCS1_OAEP.new(client_pub_key)
 
-conn.close()
+        # Отправка своего открытого ключа клиенту
+        client_socket.send(self.keys.publickey().export_key())
+
+        # Получение и расшифровка сообщения от клиента
+        encrypted_msg = client_socket.recv(2048)
+        print(f'Зашифрованное сообщение клиента: {encrypted_msg}')
+        message = self.cipher.decrypt(encrypted_msg)
+        print("расшифрованное сообщение от клиента:", message.decode())
+
+        # Отправка обратного сообщения клиенту
+        response = client_cipher.encrypt(b"hello client!")
+        client_socket.send(response)
+
+    def run(self):
+        print("Сервер запущен и слушает на", self.host, self.port)
+        while True:
+            client_socket, addr = self.server_socket.accept()
+            print(f"Подключен клиент: {addr}")
+            self.handle_client(client_socket)
+            client_socket.close()
+
+
+# Запуск сервера
+server = Server()
+server.run()
